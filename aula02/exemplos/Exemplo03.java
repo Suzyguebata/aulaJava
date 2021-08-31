fffbd65516fa17afdff5cbe9cd12d7bf056412d3
Suzyguebata <suzanaguebata@gmail.com>
2021-08-31 14:36:54 -0300

    Commit final exemplo03

--- aula02/exemplos/Exemplo03.java
+++ aula02/exemplos/Exemplo03.java
@@ -1,8 +1,25 @@
 package exemplos;
 
+import javax.print.event.PrintJobListener;
+
+//warning = aviso (amarelo)
+ //Error = erro (vernelho)
+
 public class Exemplo03 {
     public static void main(String[] args) {
-        System.out.println("Exemplo");
-        System.out.println("Exemplo02);
+        //Tipo nome;
+        int numero; //cria uma variavel chamada numero que guarda valor do tipo inteiro
+
+        numero = 10; // coloca o valor 10 dentro da variavel
+
+        System.out.println(numero);
+
+        numero = 15;
+
+        System.out.println(numero);
+
+        numero = 2 + 3 * 5;
+
+        System.out.println(numero);
     }
 }
