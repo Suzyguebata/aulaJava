29b252773d9d89cb7b1d7588d3b5a63c164eaa99
Suzyguebata <suzanaguebata@gmail.com>
2021-09-01 10:13:57 -0300

    commit scanner com condição SE

---
+++ aula03/exemplos/Exemplo01.java
@@ -0,0 +1,15 @@
+package exemplos;
+
+public class Exemplo01 {
+    public static void main(String[] args) {
+        double nota;
+
+        nota = 5.5;
+
+        if(nota >= 6){
+            System.out.println("Você foi aprovado");
+        }
+
+        System.out.println("Final do programa.");
+    }
+}
---
+++ aula03/exercicios/Exercicio01.java
@@ -0,0 +1,19 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int numero;
+
+        System.out.println("Digte seu número :");
+        numero = entrada.nextInt();
+
+        if(numero >= 20){
+            System.out.println(numero / 2);
+        }
+
+        entrada.close();
+    }
+}
