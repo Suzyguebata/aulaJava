4f5c942bca06ece154ee4a6774e11abe6d888063
Suzyguebata <suzanaguebata@gmail.com>
2021-09-02 09:42:09 -0300

    Atualização

---
+++ aula03/exemplos/Exemplo06.java
@@ -0,0 +1,11 @@
+package exemplos;
+
+public class Exemplo06 {
+    public static void main(String[] args) {
+        int a, b;
+        a = 10;
+        b = 5;
+
+        System.out.println( (a < 11) && (b > 5) );
+    }
+}
---
+++ aula03/exercicios/Exercicio08.java
@@ -0,0 +1,17 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio08 {
+    public static void main(String[] args) {
+        int a, b, c;
+        a = 2;
+        b = 2;
+        c = 4;
+
+        if( a > b + c){
+            
+        }
+
+    }
+}
