f795961f942560381d7670a28e69ece083be4328
Suzyguebata <suzanaguebata@gmail.com>
2021-09-20 19:44:19 -0300

    Validação idade

--- dotNet/aula01/Program.cs
+++ dotNet/aula01/Program.cs
@@ -6,10 +6,63 @@ namespace aula01
     {
         static void Main(string[] args)
         {
-            int idade = 35;
+            int idadeAluno;
             string nomeAluno, sobrenomeAluno;
             bool validaridade;
-            //int
+            DateTime dataNascimento;
+
+            //TryParse - Tenta fazer uma conversao, se nao for correto ele retorna
+            // False. Se for correta retorna o True e devolve o valor digitado
+             
+            /* 
+            if(int.TryParse(Console.ReadLine(),out idadeAluno))
+            {
+                Console.WriteLine("Idade do aluno: " + idadeAluno);
+            } else
+            {
+                Console.WriteLine(" *** Valor incorreto. ***");
+            }
+            */
+
+            //Try Catch
+            try
+            {
+                //Código
+                Console.Write("Digite a Idade do Aluno: ");
+                idadeAluno = int.Parse(Console.ReadLine());
+                Console.WriteLine("A idade do aluno é: " + idadeAluno);
+                dataNascimento = new DateTime(1986,01,09);
+                int auxIdade = CalcularIdade(dataNascimento);
+
+                Console.WriteLine("A idade calculada é: " + auxIdade);
+            }
+            catch(RankException ex)
+            {
+                //Controlar minha exception
+                Console.WriteLine(" *** Erro no formato da idade ***");
+            }   
+            catch(FormatException ex)
+            {
+                //Controlar minha exception
+                Console.WriteLine(" *** Erro no formato da idade ***");
+            }   
+            catch(Exception ex)
+            {
+                //Controle de erros
+                Console.WriteLine(" *** Idade incorreta. ***");
+                Console.WriteLine(ex.Message);
+                
+            }
+                  
+              /*
+            Console.Write("Digite o nome do Aluno: ");
+            nomeAluno = Console.ReadLine();
+
+            Console.Write("Digite o sobrenome do Aluno: ");
+            sobrenomeAluno = Console.ReadLine();
+
+            Console.WriteLine("Nome completo do Aluno: " + nomeAluno + " " + sobrenomeAluno);
+            */
 
             //idade = idade +1; //36 int Console.Write
 
@@ -37,30 +90,52 @@ namespace aula01
                 Tudo que estiver aqui será ignorado pelo compilador
             */
             
-            //exemplo IF
-            if (idade%2==0)
+            //Exemplo IF
+            /*
+            if(idade%2==0)
             {
-                Console.WriteLine("Número par ");
+                Console.WriteLine("Número par");
             }
             else
             {
-                Console.WriteLine("Número impar ");
+                Console.WriteLine("Número ímpar");
             }
+            */
 
-            //exemplo FOR
-            for (int i = 0; i < 5; i++)
+            //Exemplo FOR
+            /*
+            for(int i = 0; i < 5; i++)
             {
                 Console.WriteLine(i);
             }
+            */
 
-            // exemplo WHILE
+            //Exemplo WHILE
+            /*
             int x = 5;
-            while (x>0)
+            while(x > 0)
             {
-                Console.WriteLine("X: " + x--); 
+                Console.WriteLine("X: " + x--);
             }
+            */
 
-             Console.WriteLine("A minha idade é " + idade);
+            //Exemplo DO-WHILE
+            /*
+            int y = 5;
+            do
+            {
+                Console.WriteLine("Y: " + y--);
+            }while(y > 0);
+            */
+        }
+
+        static int CalcularIdade(DateTime dataNascimento)
+        {
+            int idadeAluno = -1;
+
+            idadeAluno = (DateTime.Today.Year - dataNascimento.Year);
+
+            return idadeAluno;
         }
     }
 }
