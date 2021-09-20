4fb6806a0a7289666446cffcaf8dfcb608ee5981
Suzyguebata <suzanaguebata@gmail.com>
2021-09-20 15:40:59 -0300

    C-Sharp IF, FOR e WHILE

--- dotNet/aula01/Program.cs
+++ dotNet/aula01/Program.cs
@@ -17,8 +17,50 @@ namespace aula01
 
             //++idade; // Nesse caso adicionei +1 a idade antes da leitura
 
+            /* 
+                Operadores matemáticos
 
-             Console.WriteLine("A minha idade é " + ++idade);
+                + -> Soma
+                - -> Subtração
+                / -> Divisão
+                * -> Multiplicação
+
+                % -> Resto
+
+                && -> AND
+                || -> OR
+                != -> DIFERENTE
+                == -> IGUAL
+
+            /*
+                Um bloco de comentário
+                Tudo que estiver aqui será ignorado pelo compilador
+            */
+            
+            //exemplo IF
+            if (idade%2==0)
+            {
+                Console.WriteLine("Número par ");
+            }
+            else
+            {
+                Console.WriteLine("Número impar ");
+            }
+
+            //exemplo FOR
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            // exemplo WHILE
+            int x = 5;
+            while (x>0)
+            {
+                Console.WriteLine("X: " + x--); 
+            }
+
+             Console.WriteLine("A minha idade é " + idade);
         }
     }
 }
